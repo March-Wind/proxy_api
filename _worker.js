171d4695c460db21eb6185bbc62c4d8eaa5bf1a0
March-Wind <68446969+March-Wind@users.noreply.github.com>
2024-04-15 14:07:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,12 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "api.openai.com";
+    if(request?.headers?.host === 'api.githubcopilot.com'){
+      url.host = "api.githubcopilot.com";  
+    }else{
+      url.host = "api.openai.com";  
+    }
+    
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
